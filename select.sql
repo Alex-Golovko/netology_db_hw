fd38d224d7934c37aed276bd1089aa08c703297e
Alex Golovko <ya.golovko-alex2017@yandex.ru>
2021-08-15 15:36:14 +0300

    add select file

---
+++ select.sql
@@ -0,0 +1,18 @@
+SELECT id, album_name name_, album_year year_ FROM album 
+WHERE album_year  = '2018';
+
+SELECT id, track_name track, track_lenght time_ FROM track
+WHERE track_lenght  >= (
+SELECT max(track_lenght) FROM track);
+
+SELECT id, track_name track, track_lenght time_ FROM track 
+WHERE track_lenght >= '03:30';
+
+SELECT id, collection_name collection, collection_pub_date year_ FROM collection 
+WHERE collection_pub_date > '2018' AND collection_pub_date <= '2020'; 
+
+SELECT id, singer_alias alias FROM singer 
+WHERE length(singer_alias) = 1;
+
+SELECT id, track_name FROM track 
+WHERE track_name  LIKE '%%мой%%' OR track_name LIKE '%%my%%';
