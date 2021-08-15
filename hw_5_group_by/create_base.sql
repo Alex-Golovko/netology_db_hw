992c0740dd691b4a77b772592603688d9bc7260b
Alex Golovko <ya.golovko-alex2017@yandex.ru>
2021-08-15 16:54:48 +0300

    add hw_5

---
+++ hw_5_group_by/create_base.sql
@@ -0,0 +1,47 @@
+CREATE TABLE IF NOT EXISTS collection(
+    id SERIAL PRIMARY KEY,
+    collection_name VARCHAR(40) NOT NULL,
+    collection_pub_date VARCHAR(4) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS genre(
+    id SERIAL PRIMARY KEY,
+    genre_name VARCHAR(40) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS singer(
+    id SERIAL PRIMARY KEY,
+    singer_alias VARCHAR(40) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS album(
+    id SERIAL PRIMARY KEY,
+    album_name VARCHAR(40) NOT NULL,
+    album_year VARCHAR(4) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS track(
+    id SERIAL PRIMARY KEY,
+    track_pub_date VARCHAR(40),
+    track_name VARCHAR(40) NOT NULL,
+    track_lenght TIME,
+    album_id INTEGER REFERENCES album(id)
+);
+
+CREATE TABLE IF NOT EXISTS singer_genre(
+    id SERIAL PRIMARY KEY,
+    singer_id INTEGER REFERENCES singer(id),
+    genre_id INTEGER REFERENCES genre(id)
+);
+
+CREATE TABLE IF NOT EXISTS singer_albums(
+    id SERIAL PRIMARY KEY,
+    singer_id INTEGER REFERENCES singer(id),
+    album_id INTEGER REFERENCES album(id)
+);
+
+CREATE TABLE IF NOT EXISTS collection_tracks(
+    id SERIAL PRIMARY KEY,
+    track_id INTEGER REFERENCES track(id),
+    collection_id INTEGER REFERENCES collection(id)
+);
---
+++ hw_5_group_by/insert_BD_neto0730.sql
@@ -0,0 +1,115 @@
+INSERT INTO singer (singer_alias) VALUES ('Каста');
+INSERT INTO singer (singer_alias) VALUES ('Баста');
+INSERT INTO singer (singer_alias) VALUES ('Земфира');
+INSERT INTO singer (singer_alias) VALUES ('Little Big');
+INSERT INTO singer (singer_alias) VALUES ('Zivert');
+INSERT INTO singer (singer_alias) VALUES ('SLAVA MARLOW');
+INSERT INTO singer (singer_alias) VALUES ('Сплин');
+INSERT INTO singer (singer_alias) VALUES ('Пелагея');
+
+INSERT INTO genre (genre_name) VALUES ('RAP');
+INSERT INTO genre (genre_name) VALUES ('POP');
+INSERT INTO genre (genre_name) VALUES ('FOLK');
+INSERT INTO genre (genre_name) VALUES ('ROCK');
+INSERT INTO genre (genre_name) VALUES ('PANK');
+
+INSERT INTO collection (collection_name, collection_pub_date) VALUES ( 'Хиты 2010-х', '2021');
+INSERT INTO collection(collection_name, collection_pub_date) VALUES ('Хиты 00-х', '2018');
+INSERT INTO collection(collection_name, collection_pub_date) VALUES ('Хиты 90-х', '2019');
+INSERT INTO collection(collection_name, collection_pub_date) VALUES ('Хиты 80-х', '2017');
+INSERT INTO collection(collection_name, collection_pub_date) VALUES ('Народные', '2020');
+INSERT INTO collection(collection_name, collection_pub_date) VALUES ('Музыка для машины', '2018');
+INSERT INTO collection(collection_name, collection_pub_date) VALUES ('Музыка для релакса', '2018');
+INSERT INTO collection(collection_name, collection_pub_date) VALUES ('Тяжеловато!', '2018');
+
+INSERT INTO album (album_name, album_year) VALUES ('Ты была права', '2021');
+INSERT INTO album (album_name, album_year) VALUES ('На вес золота', '2021');
+INSERT INTO album (album_name, album_year) VALUES ('Тайком', '2019');
+INSERT INTO album (album_name, album_year) VALUES ('Феллини', '2001');
+INSERT INTO album (album_name, album_year) VALUES ('Тебе', '2021');
+INSERT INTO album (album_name, album_year) VALUES ('Тропы', '2009');
+INSERT INTO album (album_name, album_year) VALUES ('Skibidi', '2019');
+INSERT INTO album (album_name, album_year) VALUES ('20', '2019');
+INSERT INTO album (album_name, album_year) VALUES ('Мой', '2020');
+
+INSERT INTO track (track_pub_date, track_name, track_lenght, album_id) 
+VALUES ('2021', 'Ты была права', '01:58', 1);
+INSERT INTO track (track_pub_date, track_name, track_lenght, album_id) 
+VALUES ('2019', 'Гена Гоин', '01:58', 8);
+INSERT INTO track (track_pub_date, track_name, track_lenght, album_id) 
+VALUES ('2019', 'Skibidi', '02:43', 7);
+INSERT INTO track (track_pub_date, track_name, track_lenght, album_id) 
+VALUES ('2021', 'На вес золота', '03:09', 2);
+INSERT INTO track (track_pub_date, track_name, track_lenght, album_id) 
+VALUES ('2021', 'Тебе', '04:43', 5);
+INSERT INTO track (track_pub_date, track_name, track_lenght, album_id) 
+VALUES ('2001', 'Феллини', '04:37', 4);
+INSERT INTO track (track_pub_date, track_name, track_lenght, album_id) 
+VALUES ('2009', 'Прелюдия', '04:28', 6);
+INSERT INTO track (track_pub_date, track_name, track_lenght, album_id) 
+VALUES ('2009', 'Колечко', '05:52', 6);
+INSERT INTO track (track_pub_date, track_name, track_lenght, album_id) 
+VALUES ('2009', 'Мамка', '05:15', 6);
+INSERT INTO track (track_pub_date, track_name, track_lenght, album_id) 
+VALUES ('2019', 'Skibidi Romantic Edition', '03:12', 7);
+INSERT INTO track (track_pub_date, track_name, track_lenght, album_id) 
+VALUES ('2019', 'Skibidi Extendet Mix', '03:09', 7);
+INSERT INTO track (track_pub_date, track_name, track_lenght, album_id) 
+VALUES ('2019', 'Тайком', '03:45', 3);
+INSERT INTO track (track_pub_date, track_name, track_lenght, album_id) 
+VALUES ('2019', 'Атом', '04:56', 3);
+INSERT INTO track (track_pub_date, track_name, track_lenght, album_id) 
+VALUES ('2019', 'Гимн', '06:10', 3);
+INSERT INTO track (track_pub_date, track_name, track_lenght, album_id) 
+VALUES ('2019', 'Воздушный шар', '03:26', 3);
+INSERT INTO track (track_pub_date, track_name, track_lenght, album_id) 
+VALUES ('2020', 'Мой', '04:35', 11);
+
+INSERT INTO singer_genre (singer_id, genre_id) VALUES (1, 1);
+INSERT INTO singer_genre (singer_id, genre_id) VALUES (2, 1);
+INSERT INTO singer_genre (singer_id, genre_id) VALUES (3, 4);
+INSERT INTO singer_genre (singer_id, genre_id) VALUES (4, 2);
+INSERT INTO singer_genre (singer_id, genre_id) VALUES (5, 2);
+INSERT INTO singer_genre (singer_id, genre_id) VALUES (6, 1);
+INSERT INTO singer_genre (singer_id, genre_id) VALUES (7, 4);
+INSERT INTO singer_genre (singer_id, genre_id) VALUES (8, 3);
+
+INSERT INTO singer_albums (singer_id, album_id ) VALUES (2, 1);
+INSERT INTO singer_albums (singer_id, album_id ) VALUES (2, 2);
+INSERT INTO singer_albums (singer_id, album_id ) VALUES (7, 3);
+INSERT INTO singer_albums (singer_id, album_id ) VALUES (7, 4);
+INSERT INTO singer_albums (singer_id, album_id ) VALUES (5, 5);
+INSERT INTO singer_albums (singer_id, album_id ) VALUES (8, 6);
+INSERT INTO singer_albums (singer_id, album_id ) VALUES (4, 7);
+INSERT INTO singer_albums (singer_id, album_id ) VALUES (6, 8);
+
+INSERT INTO collection_tracks (track_id, collection_id)
+VALUES (1, 1);
+INSERT INTO collection_tracks (track_id, collection_id)
+VALUES (2, 1);
+INSERT INTO collection_tracks (track_id, collection_id)
+VALUES (3, 1);
+INSERT INTO collection_tracks (track_id, collection_id)
+VALUES (4, 1);
+INSERT INTO collection_tracks (track_id, collection_id)
+VALUES (5, 1);
+INSERT INTO collection_tracks (track_id, collection_id)
+VALUES (6, 2);
+INSERT INTO collection_tracks (track_id, collection_id)
+VALUES (7, 2);
+INSERT INTO collection_tracks (track_id, collection_id)
+VALUES (8, 2);
+INSERT INTO collection_tracks (track_id, collection_id)
+VALUES (9, 2);
+INSERT INTO collection_tracks (track_id, collection_id)
+VALUES (10, 1);
+INSERT INTO collection_tracks (track_id, collection_id)
+VALUES (11, 1);
+INSERT INTO collection_tracks (track_id, collection_id)
+VALUES (12, 1);
+INSERT INTO collection_tracks (track_id, collection_id)
+VALUES (13, 1);
+INSERT INTO collection_tracks (track_id, collection_id)
+VALUES (14, 1);
+INSERT INTO collection_tracks (track_id, collection_id)
+VALUES (15, 1);
